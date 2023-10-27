ec6548741df38d4b07370e992a74a5cbc748d473
Thales <thales.souz@outlook.com>
2023-10-27 12:27:13 -0300

    fix(auth): signin and signup fixed

--- server/src/main/java/gallery/domain/entities/User.java
+++ server/src/main/java/gallery/domain/entities/User.java
@@ -58,14 +58,6 @@ public class User implements UserDetails {
           return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getUsername() {
         return username;
     }
@@ -74,28 +66,13 @@ public class User implements UserDetails {
         this.username = username;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserRole role) {
-        this.role = role;
 
     @Override
     public boolean isAccountNonExpired() {
